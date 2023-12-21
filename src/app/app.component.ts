29b034a27221ccc44d1a9a5d69bae2ef552d31cd
Corn <11050034@me.mcu.edu.tw>
2023-12-21 23:42:01 +0800

    feat(nav-bar): 導覽列元件建立與實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [NavBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'conduit';
-}
+export class AppComponent {}
