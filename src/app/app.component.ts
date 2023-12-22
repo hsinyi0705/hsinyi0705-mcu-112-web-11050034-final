edfa155147c1c7a77bad5fb61ce0b3ce275c0b67
Corn <11050034@me.mcu.edu.tw>
2023-12-22 14:33:23 +0800

    feat(header): 標題列元件建立與實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
