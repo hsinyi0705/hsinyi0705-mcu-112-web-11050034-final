529556decec03c98c5e7f9056198cbfb04a889c2
Corn <11050034@me.mcu.edu.tw>
2024-01-05 16:37:25 +0800

    feat(footer): 頁尾列元件建立與實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
+import { Component } from '@angular/core';
+
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent, HeaderComponent],
+  imports: [NavBarComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
