45b09b3ab4cebb97db0c3e3a2f445019ba6ed041
Corn <11050034@me.mcu.edu.tw>
2024-01-11 23:31:16 +0800

    feat(model): 文章資料類別定義新增

--- src/app/article-preview/article-preview.component.ts
+++ src/app/article-preview/article-preview.component.ts
@@ -1,18 +1,36 @@
-import { NgFor } from '@angular/common';
+import { DatePipe, NgFor } from '@angular/common';
 import { Component, HostBinding } from '@angular/core';
 
 import { TagListComponent } from '../tag-list/tag-list.component';
 import { FavoriteButtonComponent } from '../favorite-button/favorite-button.component';
+import { Article } from '../model/article';
 
 @Component({
   selector: 'app-article-preview',
   standalone: true,
-  imports: [FavoriteButtonComponent, TagListComponent],
+  imports: [DatePipe, FavoriteButtonComponent, TagListComponent],
   templateUrl: './article-preview.component.html',
   styleUrl: './article-preview.component.css',
 })
 export class ArticlePreviewComponent {
   @HostBinding('class') class = 'article-preview';
 
-  tags = ['enim', 'repellat', 'exercitationem', 'est'];
+  article = new Article({
+    id: 1,
+
+    title:
+      'Ill quantify the redundant TCP bus, that should hard drive the ADPbandwidth!',
+
+    content: `Aut facilis qui. Cupiditate sit ratione eum sunt rerum impedit. Qui suscipit
+    debitis et et voluptates voluptatem voluptatibus. Quas voluptatum quae
+    corporis corporis possimus.`,
+
+    favoriteCount: 30,
+
+    author: 'Maksim Esteban',
+
+    createDate: new Date(2024, 6, 4),
+
+    tags: ['enim', 'repellat', 'exercitationem', 'est'],
+  });
 }
