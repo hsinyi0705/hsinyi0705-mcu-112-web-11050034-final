72eadc5fb7d08ff8517b0743477d0f08bfb2e2e0
Corn <11050034@me.mcu.edu.tw>
2024-01-11 14:09:51 +0800

    feat(article-preview): 文章預覽區塊 header 建立與實作

---
+++ src/app/article-preview/article-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-preview.component.html',
+  styleUrl: './article-preview.component.css'
+})
+export class ArticlePreviewComponent {
+
+}
