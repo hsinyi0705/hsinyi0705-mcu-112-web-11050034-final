b0d9e5d85b0e3b128e7b76d79b356645e1a2f83f
Corn <11050034@me.mcu.edu.tw>
2024-01-12 08:06:53 +0800

    chore(json-server): 設定 json-server 套件

--- src/app/article-preview/article-preview.component.ts
+++ src/app/article-preview/article-preview.component.ts
@@ -1,24 +1,30 @@
-import { Component, HostBinding } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, HostBinding, inject } from '@angular/core';
 
 import { ArticleListComponent } from '../article-list/article-list.component';
 import { Article } from '../model/article';
+import { ArticleService } from './../services/article.service';
 
 @Component({
   selector: 'app-article-preview',
   standalone: true,
-  imports: [ArticleListComponent],
+  imports: [AsyncPipe, JsonPipe, ArticleListComponent],
   templateUrl: './article-preview.component.html',
   styleUrl: './article-preview.component.css',
 })
 export class ArticlePreviewComponent {
+  private readonly articleService = inject(ArticleService);
+
   @HostBinding('class') class = 'article-preview';
+
+  articles$ = this.articleService.getList();
 
   articles = [
     new Article({
       id: 1,
 
       title:
-        'Ill quantify the redundant TCP bus, that should hard drive the ADPbandwidth!',
+        'Ill quantify the redundant TCP bus, that should hard drive the ADP bandwidth!',
 
       content: `Aut facilis qui. Cupiditate sit ratione eum sunt rerum impedit. Qui suscipit
     debitis et et voluptates voluptatem voluptatibus. Quas voluptatum quae
@@ -37,7 +43,7 @@ export class ArticlePreviewComponent {
       id: 2,
 
       title:
-        'Ill quantify the redundant TCP bus, that should hard drive the ADPbandwidth!',
+        'Ill quantify the redundant TCP bus, that should hard drive the ADP bandwidth!',
 
       content: `Aut facilis qui. Cupiditate sit ratione eum sunt rerum impedit. Qui suscipit
       debitis et et voluptates voluptatem voluptatibus. Quas voluptatum quae
---
+++ src/app/services/article.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Article } from '../model/article';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ArticleService {
+  private readonly url = 'http://localhost:3000/article';
+
+  private readonly httpClient = inject(HttpClient);
+
+  getList(): Observable<Article[]> {
+    return this.httpClient.get<Article[]>(this.url);
+  }
+}
