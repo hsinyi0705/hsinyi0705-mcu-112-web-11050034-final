085f180d1a455ebfbe2d3e0a3d8c8faa9f53e6ba
Corn <11050034@me.mcu.edu.tw>
2024-01-14 17:18:04 +0800

    feat(article): 文章內容頁面樣式實作

--- src/app/article-content-page/article-content-page.component.ts
+++ src/app/article-content-page/article-content-page.component.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 import { Article } from '../model/article';
+import { ArticleMetaComponent } from '../article-meta/article-meta.component';
+import { TagListComponent } from '../tag-list/tag-list.component';
 
 @Component({
   selector: 'app-article-content-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ArticleMetaComponent, TagListComponent],
   templateUrl: './article-content-page.component.html',
   styleUrl: './article-content-page.component.css',
 })
